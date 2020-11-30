2a4598a3bc83a24f04dabd34447a2e3ba29e3ba9
zainzafar90 <zain@polly.ai>
2020-11-30 16:51:33 +0500

    Remove Gilroy font css link

--- pages/_document.js
+++ pages/_document.js
@@ -12,10 +12,6 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-          <link
-            rel="stylesheet"
-            href="https://cdn.rawgit.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css"
-          />
         </Head>
 
         <body className="bg-gray-50 font-sans antialiased text-gray-500">
